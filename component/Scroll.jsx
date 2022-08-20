662533fc2c1c710f2d5b2612ef9b3e89197c66d2
Karishma282 <karishmanikam282@gmail.com>
2022-08-21 00:30:42 +0530

    complete

--- component/Scroll.jsx
+++ component/Scroll.jsx
@@ -5,89 +5,63 @@ import { Link } from 'react-router-dom';
 const Scroll = () => {
   const [data, setData] = useState([]);
   const [items, setItems] = useState([]);
+  const [page, setPage] = useState(1);
 
   const [pageCount, setpageCount] = useState(0);
 
   let limit = 10;
 
   useEffect(() => {
-    fetch('https://scroller-app.herokuapp.com/api/launchesPast')
+    fetch(
+      `https://scroller-app.herokuapp.com/api/launchesPast?_page=${page}&_limit=10`,
+    )
       .then((res) => res.json())
       .then((res) => setData(res))
       .catch((err) => console.log(err));
-  });
-  useEffect(() => {
-    const getdata = async () => {
-      const res = await fetch(
-        `https://scroller-app.herokuapp.com/api/launchesPast=${limit}`,
-      );
-      const data = await res.json();
-      const total = res.headers.get('x-total-count');
-      setpageCount(Math.ceil(total / limit));
-      // console.log(Math.ceil(total/12));
-      setItems(data);
-    };
+  }, [page]);
 
-    getdata();
-  }, [limit]);
-  const fetchdata = async (currentPage) => {
-    const res = await fetch(
-      `https://scroller-app.herokuapp.com/api/launchesPast=${currentPage}&_limit=${limit}`,
-    );
-    const data = await res.json();
-    return data;
-  };
+  data.sort((a, b) => a.id - b.id);
 
-  const handlePageClick = async (data) => {
-    console.log(data.selected);
-
-    let currentPage = data.selected + 1;
-
-    const commentsFormServer = await fetchdata(currentPage);
-
-    setItems(commentsFormServer);
-  };
   return (
-    <div
-      style={{
-        marginTop:"10px",
-        width: '50%',
-        height: '500px',
-        textAlign: 'center',
-        overflow: 'scroll',
-        margin: 'auto',
-        textDecoration: 'none',
-      }}>
-      {data.map((item) => {
-        return (
-          <Link to={`/data/${item.id}`} key={item.id}>
-            <div key={item.id}>
-              <h3 style={{ textDecoration: 'none', color: 'black' }}>
-                mission_name :- {item.mission_name}
-              </h3>
-            </div>
-          </Link>
-        );
-      })}
-      <ReactPaginate
-        previousLabel={'previous'}
-        nextLabel={'next'}
-        breakLabel={'...'}
-        pageCount={pageCount}
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={3}
-        onPageChange={handlePageClick}
-        containerClassName={'pagination justify-content-center'}
-        pageClassName={'page-item'}
-        pageLinkClassName={'page-link'}
-        previousClassName={'page-item'}
-        previousLinkClassName={'page-link'}
-        nextClassName={'page-item'}
-        nextLinkClassName={'page-link'}
-        breakClassName={'page-item'}
-        breakLinkClassName={'page-link'}
-        activeClassName={'active'}
-      />
+    <div >
+      <h1 style={{marginTop:'1rem',textAlign:'center',color:'teal'}}>INFINITY - SCROLLING</h1>
+      <div
+        style={{
+          width: '40%',
+          height: '350px',
+          textAlign: 'center',
+          overflow: 'scroll',
+          margin: 'auto',
+          textDecoration: 'none',
+        }}>
+        {data.map((item) => {
+          return (
+            <Link to={`/data/${item.id}`} key={item.id}>
+              <div
+                style={{ display: 'flex', gap: '20px', marginLeft: '50px' }}
+                key={item.id}>
+                <h3>{item.id}</h3>
+                <h3 style={{ textDecoration: 'none', color: 'black' }}>
+                  mission_name :- {item.mission_name}
+                </h3>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+
+      <div style={{marginLeft:'500px',width:'30',gap:'1em',marginTop:'1rem'}}>
+        <button
+          disabled={page == 1 ? true : false}
+          onClick={() => (page > 1 ? setPage(page - 1) : setPage(page))}>
+          Pre
+        </button>
+        <button
+          disabled={page == 10 ? true : false}
+          onClick={() => (page < 10 ? setPage(page + 1) : setPage(page))}>
+          Next
+        </button>
+      </div>
     </div>
   );
 };
